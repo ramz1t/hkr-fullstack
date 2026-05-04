27fe69f60788098e6b91885b91b0061c6f4144d1
simonostini <simonostini@gmail.com>
2026-05-04 12:10:00 +0200

    feat: added swagger documentation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  const configService = app.get(ConfigService);
+  const isDevelopment = configService.get<string>("NODE_ENV") !== "production";
+  const port = configService.get<number>("API_PORT") ?? 4000;
+  const config = new DocumentBuilder()
+    .setTitle("Casino API")
+    .setDescription("Casino API documentation")
+    .setVersion("1.0")
+    .build();
+
+  if (isDevelopment) {
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("swagger", app, documentFactory);
+    console.log(`Swagger running at http://localhost:${port}/swagger`);
+  }
+
+  await app.listen(port);
+
+  const url = `http://localhost:${port}`;
+  console.log(`🚀 API running at: ${url}`);
 }
-bootstrap();
+
+bootstrap().catch(console.log);
