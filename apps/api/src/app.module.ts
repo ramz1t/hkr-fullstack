32012d4c8b2b60dd92f4130fa651f5a90fd9a5d0
simonostini <simonostini@gmail.com>
2026-05-04 12:17:12 +0200

    fix: web project ports and removed api boilerplate, updated env example

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { ConfigModule } from "@nestjs/config";
-import { AppService } from "./app.service";
 
 @Module({
   imports: [
@@ -11,6 +10,6 @@ import { AppService } from "./app.service";
     })
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: []
 })
 export class AppModule {}
