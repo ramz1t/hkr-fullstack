312a1dc74536b54113a935cf39c3106b91befb55
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2026-05-11 13:07:25 +0200

    feat: migrated web apps from next to react

---
+++ apps/web-admin/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/button";
+
+function App() {
+  return (
+    <div>
+      Web Public <Button>Test</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/web-public/src/main.tsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import "./index.css";
+import App from "./App.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retryDelay: 0,
+      retry: 0,
+      refetchOnWindowFocus: false
+    }
+  }
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter basename="/">
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <App />
+          <ReactQueryDevtools />
+          <Toaster
+            containerClassName="max-md:!top-20 max-md:!inset-x-2"
+            position="top-right"
+          />
+        </HelmetProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
