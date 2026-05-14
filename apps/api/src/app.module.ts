cf263f69967548d081d45acf8b382f4223f82bed
simonostini <simonostini@gmail.com>
2026-05-14 22:21:15 +0200

    refactor: Migrate shared env to per app env

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -14,7 +14,7 @@ import { PipesModule } from "./common/pipes/pipes.module";
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: "../../.env",
+      envFilePath: "../.env",
       isGlobal: true
     }),
     DatabaseModule,
